d2b4acdd7af85495669adfec89444ae1f49a39ab
Bhairav chaudhari <nishant.taksande@herolifeinsurance.ca>
2024-06-11 16:03:29 +0530

    deleted:    cypress/downloads/downloads.htm 	new file:   cypress/e2e/letcode.cy.js

---
+++ cypress/e2e/letcode.cy.js
@@ -0,0 +1,20 @@
+describe('user sign up & login flow', () =>{
+    it('should sign up successfully', ()=>{
+        cy.visit("https://letcode.in/test");
+        cy.get('.navbar-burger').click();
+        cy.xpath("//a[normalize-space()='Sign up']").click();
+        cy.get("#name").type("Dinesh kolati");
+        cy.get("#email").type("dinesh.kolati@mailinator.com");
+        cy.get("#pass").type("Dinesh@123");
+        cy.get("#agree").check();
+        cy.xpath("//button[text()='SIGN UP']").click();
+    })
+    it('Login',()=>{
+        cy.visit("https://letcode.in/test");
+        cy.get('.navbar-burger').click();
+        cy.xpath("//a[text()='Log in']").click();
+        cy.get("[name='email']").type("dinesh.kolati@mailinator.com");
+        cy.get("[name='password']").type("Dinesh@123");
+        cy.xpath("//button[text()='LOGIN']").click();
+    })
+})
