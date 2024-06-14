bc118ef278c2147dfcb4f349882fb1d79729f29e
Bhairav chaudhari <nishant.taksande@herolifeinsurance.ca>
2024-06-14 18:29:09 +0530

    deleted:    cypress/downloads/downloads.htm 	modified:   cypress/e2e/dropdowns.cy.js 	modified:   cypress/screenshots/dropdowns.cy.js/static dropdown -- Dropdown without select (failed).png 	deleted:    cypress/screenshots/dropdowns.cy.js/static dropdown -- Dynamic dropdown (failed).png

--- cypress/e2e/dropdowns.cy.js
+++ cypress/e2e/dropdowns.cy.js
@@ -24,26 +24,19 @@ describe('static dropdown', () => {
     })
 
     //working
-    it('Autosuggest dropdown', function () {
-       cy.visit('https://www.wikipedia.org/')
-        cy.get("#searchInput").type("Delhi")
-        cy.get('.suggestion-title').contains('Delhi University').click()
-    })
-
-    //not working
     it('Dynamic dropdown', function () {
         cy.visit('https://www.google.com/')
-        cy.get("input[name='q']").type('cypress automation');
+        cy.get("#APjFqb").type('cypress automation');
         cy.wait(3000)
-        cy.get('div.wM6W7d>span').should('have.length',11)
+        cy.get('div.wM6W7d>span').should('have.length',13)
 
         cy.get("div.wM6W7d>span").each(($el, index,$list)=> {
-            if($el.text()=='cypress automation tool')
+            if($el.text()=='cypress automation')
             {
                 cy.wrap($el).click()
             }
          })
-       cy.get("input[name='q']").should("have.value","cypress automation tool")
+       cy.get("#APjFqb").should("have.value","cypress automation")
         
      })
 })
